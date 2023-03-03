970360945fcc12a09c01c97bea5d1c1878611850
Anonym <anonym516836@gmail.com>
2023-03-03 21:19:28 +0300

    Task 1_2_0

--- Lesson_1/1_2/Program.cs
+++ Lesson_1/1_2/Program.cs
@@ -7,3 +7,4 @@ if(s_a == "4") Console.WriteLine("Четверг");
 if(s_a == "5") Console.WriteLine("Пятница");
 if(s_a == "6") Console.WriteLine("Суббота");
 if(s_a == "7") Console.WriteLine("Воскресенье");
+else Console.WriteLine("Ошибка");
