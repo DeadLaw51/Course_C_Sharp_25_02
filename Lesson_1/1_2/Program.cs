78b56dd3d0bf9c246e559d043b19a9e38920d4e1
Anonym <anonym516836@gmail.com>
2023-03-03 21:01:10 +0300

    Task 1_2

---
+++ Lesson_1/1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿string s_a = Console.ReadLine()!;
+
+if(s_a == "1") Console.WriteLine("Понедельник");
+if(s_a == "2") Console.WriteLine("Вторник");
+if(s_a == "3") Console.WriteLine("Среда");
+if(s_a == "4") Console.WriteLine("Четверг");
+if(s_a == "5") Console.WriteLine("Пятница");
+if(s_a == "6") Console.WriteLine("Суббота");
+if(s_a == "7") Console.WriteLine("Воскресенье");
