c7f77b2ce7e176cb546f18a6c29f24228353c888
Anonym <anonym516836@gmail.com>
2023-03-03 22:10:22 +0300

    HW_S_1_2

---
+++ HomeWork/Semenar_1/1_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+
+int rezult = num % 2;
+
+if (rezult > 0 ){
+    Console.WriteLine("No");
+}
+else Console.WriteLine("Yes");
